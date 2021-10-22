9353dec404d4c0c1efaf79cae1f965b9eb651f09
Ana Paula T. Costa <77856562+anpaulat@users.noreply.github.com>
2021-10-22 20:14:28 -0300

    views

---
+++ login.php
@@ -0,0 +1,11 @@
+<div style="background-color:coral;margin:10px">
+</div>  
+
+<div style="width: 100%; margin-left: 25%">
+    <form action="index.php?rota=Login" method="post">
+        <input type="text" name="usuario" placeholder="Usuário">
+        <input type="text" name="senha" placeholder="Senha">
+        <input type="submit" name="login" value="Login">
+    </form>    
+    <br/>
+</div>
