bdc8e92c3e293aa3569d5b1cb75547a609252cc3
Ana Paula T. Costa <77856562+anpaulat@users.noreply.github.com>
2021-10-22 20:21:36 -0300

    views

---
+++ views/pizzalian.php
@@ -0,0 +1,194 @@
+
+	<div class="imagem">
+		<div class="ima">
+			<figure id="slide">
+				<img src="assets/imagens/revslider_h1_bg-1.jpg"/>
+				<img src="assets/imagens/revslider_h1_bg-2.jpg"/>
+				<img src="assets/imagens/revslider_h1_bg-3.jpg"/>
+			</figure>
+		</div>
+		<i aria-hidden="true" class="piizalian-icon- piizalian-icon-parten iconImg"></i>
+		<div class="fontImg1">
+			<span>HAPPY HOUR SPECIAL</span>
+		</div>
+		<div class="fontBevan">
+			<span> Share Your</span>
+		</div>
+		<div class="fontBevan2">    
+			<span> Love For Piizalian!</span>
+		</div>
+		<div class="menu3">
+			<a href="#" class="box2">
+				<i aria-hidden="true" class="iconImg2 piizalian-icon- piizalian-icon-cart"></i>
+				<span class="font166002">Order Now</span>
+			</a>
+		</div> 
+	</div>
+	<br></br>
+	<div>
+		<span class="fontChoose">CHOOSE YOUR FLAVOR</span>
+	</div>
+	<br></br>
+	<div>
+		<section class="grid grid-template-columns-1">
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/p1-480x480.jpg">
+					<span class="fonteImagens">FANTASTIC IRON SHOES</span><br>
+					<span class="descricaoImagens">Pizza, Uncategorized</span><br>
+					<span class="fonteImagensBordo">$101.00 – $187.00</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Select options</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/p10-480x480.jpg">
+					<span class="fonteImagens">SLEEK IRON CLOCK</span><br> 
+					<span class="descricaoImagens">Pizza, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$107.00 – $198.00</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Select options</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/p2-480x480.jpg">
+					<span class="fonteImagens">GORGEOUS SILK PLATE</span><br> 
+					<span class="descricaoImagens">Pizza, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$100.00 – $194.00</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Select options</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/k3-480x480.jpg">
+					<span class="fonteImagens">INTELLIGENT WOODEN TABLE</span><br> 
+					<span class="descricaoImagens">Kebab, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$134.04</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Add to cart</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/s1-480x480.jpg">
+					<span class="fonteImagens">RUSTIC MARBLE LAMP</span><br> 
+					<span class="descricaoImagens">Salads, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$154.45</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Select options</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/p3-480x480.jpg">
+					<span class="fonteImagens">FANTASTIC MARBLE BAG</span><br> 
+					<span class="descricaoImagens">Pizza, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$104.00 – $198.00</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Add to cart</span>
+					</span>
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/prod201-480x480.png">
+					<span class="fonteImagens">MEDIOCRE ALUMINUM SHIRT</span><br> 
+					<span class="descricaoImagens">Drinks, Uncategorized</span><br>  
+					<span class="fonteImagensBordo">$172.88</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Add to cart</span>
+					</span> 
+				</a>
+			</li>
+			<li style="list-style: none;">
+				<a href="#" class="item">
+					<img src="assets/imagens/prod2010-480x480.png">
+					<span class="fonteImagens">AERODYNAMIC IRON TABLE</span><br> 
+					<span class="descricaoImagens">Drinks, Uncategorized</span><br> 
+					<span class="fonteImagensBordo">$51.11</span>
+					<span class="boxImg2">
+						<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+						<span>Add to cart</span>
+					</span>
+				</a>
+			</li>
+		</section>
+	</div>
+	<br></br>
+	<div>
+		<section class="grid2 grid2-template-columns-1">
+			<div class="item2">
+				<div class="container2">
+					<div>
+						<span class="fontItem2">SUPPER, DELICIOUS PIZZA</span><br>
+						<span class="fontItem22">Feel Hunger! Order Your Favouriate Food.</span>
+					</div>
+					<div class="menuImg">
+						<a href="#" class="boxImg">
+							<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+							<span style="color: #ffffff;">Order Now</span>
+						</a>
+					</div> 
+					<img src="assets/imagens/h3_pizza-1.png">
+				</div>
+			</div>
+		</section>
+	</div>
+	<br></br>
+	<div>
+		<span class="fontChoose">GOES GREAT WITH
+		</span>
+	</div>
+	<br></br>
+	<div class="container3">
+		<div class="row"> 
+			<div class="margemImg col order-first">
+				<img src="assets/imagens/h3_pizza-2.png">
+			</div>
+			<div class="col order-last">
+				<span class="fontItem22-">ALWAYS FRESH</span><br> 
+				<span class="fontItem2" style="color: #ffffff">GO AHEAD AND BUILD YOUR OWN PIZZA WE WON’T JUDGE!</span><br> 
+				<span class="fontItem22" style="color: #ffffff">Feel Hunger! Order Your Favouriate Food.</span><br>
+				<a href="#" class="boxImg">
+					<i aria-hidden="true" class="piizalian-icon- piizalian-icon-cart" style="color: #ffffff;"></i>
+					<span style="color: #ffffff;">Order Now</span>
+				</a>
+			</div> 
+		</div>
+	</div>
+</div>
+<br></br>
+</div>
+<script>
+	$(function(){
+		$('#slide img:eq(0)').addClass("ativo").show();
+		setInterval(slide,4000);
+		function slide(){
+			if($('.ativo').next().length){
+				$('.ativo').fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+
+			}else{ 
+				$('.ativo').fadeOut().removeClass("ativo");
+				$('#slide img:eq(0)').fadeIn().addClass("ativo");
+
+			}
+
+		}
+	});
+</script>
+</html>
